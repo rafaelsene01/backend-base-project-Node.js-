dc567c6fe79ede53ec02ba42951e55f0105d09f7
rafaelsene01 <rafaelssene@gmail.com>
2019-06-25 02:35:23 -0300

    config models/User.js, table and tested connection to DB

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'hello world' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Rafael Sene',
+    email: 'rafael@gmail.com',
+    password_hash: 'dfswe',
+  });
+  return res.json(user);
+});
 
 export default routes;
